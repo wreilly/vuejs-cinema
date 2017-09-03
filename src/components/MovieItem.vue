1a7afbf24be4c6c5f95f8d39fad58a904d28d502
William Reilly <wreilly2001@gmail.com>
2017-09-03 07:21:23 -0400

    L86 MovieItem props: HTML kebab, props camel

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Here in MovieItem, FOOB our movie has a title: {{ movieItemThingFooBar.movie.Title }}
+    </div>
+</template>
+
+<script>
+    export default {
+/*
+     Vue Magic:
+        1) You use kebab-case over in SENDING Component's template/markup/HTML.
+    2) Vue "magically" gives you camelCase of SAME THING, HERE in receiving Component's props: ['camelCase'].
+    Cheers.
+        https://vuejs.org/v2/guide/components.html#camelCase-vs-kebab-case
+*/
+    props: ['movieItemThingFooBar'] // all we got on that movie ...
+    }
+</script>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,7 +1,29 @@
 <template>
     <div id="movie-list">
-        <div v-for="movie in filteredMovies" class="movie">Filtered Movie! {{ movie.movie.Title }}</div>
-        <div>
+<!--  WR__ FIRST GO:
+
+        <div v-for="movie in filteredMovies" class="movie">
+            <p>
+                Filtered Movie! {{ movie.movie.Title }}
+            </p>
+&lt;!&ndash; Vue Magic:
+1) You use kebab-case HERE in template/markup/HTML.
+2) Vue "magically" gives you camelCase of SAME THING over in receiving Component's props: ['camelCase'].
+Cheers.
+            https://vuejs.org/v2/guide/components.html#camelCase-vs-kebab-case
+&ndash;&gt;
+            <movie-item v-bind:movie-item-thing-foo-bar="movie"></movie-item>
+        </div>
+-->
+
+
+        <!-- WR__ SECOND GO (per INSTRUCTOR APPROACH. Va bene.
+         Basically, drop the div wrapper, make it directly the child component movie-item that you put the v-for directive on, the prop, etc.
+         Cheers.
+         -->
+        <movie-item v-for="movie in filteredMovies" class="movie" v-bind:movie-item-thing-foo-bar="movie"></movie-item>
+         <div>
+
             <p>Hey Checked Genres Why Not</p>
             <div v-for="genre in genresmylist">Checked Genre Title: {{genre}}</div>
         </div>
@@ -10,6 +32,7 @@
 
 <script>
     import myFrozenGenres from '../util/genres'
+    import MovieItem from './MovieItem.vue'
 
     export default {
         /* Ah. Problem. (again)
@@ -34,8 +57,23 @@ which comes as props from Root instance where it is known as 'moviesFromAPI'. Ch
 */
             }
         },
+        components: {
+            'movie-item': MovieItem,
+            // NOPE: >> MovieItem: 'movie-item'
+            /* Yeah, yeah. INSTRUCTOR CODE:
+            // ES6:
+            MovieItem
+            //  ES6. Hmmph. Works great. Sheesh.
+
+            // More "Vue Magic"
+            // Automatically gives you "ProperCase - to - kebab-case" transmogrification. Cain't beat it.
+            */
+        },
         computed: {
             filteredMovies() {
+                // -----------
+                // SEE "INSTRUCTOR CODE" at BOTTOM. Gracias.
+                // -----------
                 if (this.genresmylist.length == 0) {
                     return this.moviesForMovieList
                 } else {
@@ -49,6 +87,41 @@ which comes as props from Root instance where it is known as 'moviesFromAPI'. Ch
                     })
                 }
             }
+            /*
+            INSTRUCTOR CODE
+            for computed() filteredMovies()
+
+            Essentially:
+            1) OUTER is Movies; uses .FILTER()
+            2) "INNER" is another method, on GENRES; uses .FIND()
+
+            ------------
+             &&&&&&&&&&&&&
+             LESSON 80
+             CREATING LOGIC FOR GENRE FILTER
+
+
+             Instructor Code:
+             https://github.com/vuejsdevelopers/vuejs-cinema/blob/lecture/080/src/main.js
+             ------------
+             methods: {
+             moviePassesGenreFilter(movie) {
+             if (!this.genre.length) {
+             return true;
+             } else {
+             return this.genre.find(genre => movie.genre === genre);
+             }
+             }
+             },
+             computed: {
+             filteredMovies() {
+             return this.movies.filter(this.moviePassesGenreFilter);
+             }
+             }
+             },
+             ------------
+            ------------
+             */
         }
     }
 </script>
